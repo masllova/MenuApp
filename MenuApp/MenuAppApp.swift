837c3cbfba18100efbf70085ca7d2e86cf7c50fa
alexa <allexa.msv@gmail.com>
2023-07-01 21:44:11 +0300

    Initial Commit

---
+++ MenuApp/MenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppApp.swift
+//  MenuApp
+//
+//  Created by Александра Маслова on 01.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
